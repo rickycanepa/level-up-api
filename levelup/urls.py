2bb803290349876ca8e3f9ea93742f7be0ec9587
Ricky Canepa <rickycanepa.rc@gmail.com>
2023-02-16 01:21:28 -0600

    added event view and routed url

--- levelup/urls.py
+++ levelup/urls.py
@@ -4,11 +4,12 @@ from django.urls import path
 from levelupapi.views import register_user, login_user
 from django.conf.urls import include
 from rest_framework import routers
-from levelupapi.views import GameTypeView, GameView
+from levelupapi.views import GameTypeView, GameView, EventView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
 router.register(r'games', GameView, 'game')
+router.register(r'events', EventView, 'event')
 
 urlpatterns = [
     path('register', register_user),
---
+++ levelupapi/views/event.py
@@ -0,0 +1,38 @@
+"""View module for handling requests about game types"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from levelupapi.models import Event
+
+
+class EventView(ViewSet):
+    """Level up event view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single event
+
+        Returns:
+            Response -- JSON serialized event
+        """
+        event = event.objects.get(pk=pk)
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+
+
+    def list(self, request):
+        """Handle GET requests to get all events
+
+        Returns:
+            Response -- JSON serialized list of events
+        """
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+    
+class GameTypeSerializer(serializers.ModelSerializer):
+    """JSON serializer for game types
+    """
+    class Meta:
+        model = Event
+        fields = ('id', 'date_of_event', 'start_time', 'location', 'game', 'host', 'attendees')
